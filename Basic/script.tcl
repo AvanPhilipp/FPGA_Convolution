997936657a1a4b154449aae7532b2ecf4100a68a
Kóta Fülöp <thefealoce@gmail.com>
2017-11-30 16:05:08 +0100

    testbench update

--- Basic/script.tcl
+++ Basic/script.tcl
@@ -3,21 +3,20 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2016 Xilinx, Inc. All Rights Reserved.
 ############################################################
-open_project Convolution
-add_files Convolution/design/template/constants.h
-add_files Convolution/design/template/convolution_template.h
-add_files Convolution/design/template/fully_connected_template.h
-add_files Convolution/design/network.cpp
-add_files Convolution/design/network.h
-add_files Convolution/design/template/pooling_template.h
-add_files -tb Convolution/test_bench/convolution_tb.h
-add_files -tb Convolution/test_bench/fully_connected_tb.h
-add_files -tb Convolution/test_bench/main_tb.cpp
-add_files -tb Convolution/test_bench/pooling_tb.h
+open_project FPGA_Convolution
+add_files FPGA_Convolution/design/template/constants.h
+add_files FPGA_Convolution/design/template/convolution_template.h
+add_files FPGA_Convolution/design/template/fully_connected_template.h
+add_files network.cpp
+add_files FPGA_Convolution/design/network.h
+add_files FPGA_Convolution/design/template/pooling_template.h
+add_files -tb FPGA_Convolution/test_bench/fully_connected_tb.h
+add_files -tb FPGA_Convolution/test_bench/main_tb.cpp
+add_files -tb FPGA_Convolution/test_bench/pooling_tb.h
 open_solution "Basic"
 set_part {xc7z020clg484-1} -tool vivado
 create_clock -period 10 -name default
-#source "./Convolution/Basic/directives.tcl"
+#source "./FPGA_Convolution/Basic/directives.tcl"
 csim_design -compiler gcc
 csynth_design
 cosim_design
--- test_bench/main_tb.cpp
+++ test_bench/main_tb.cpp
@@ -1,10 +1,111 @@
 #include <stdio.h>
-#include "convolution_tb.h"
-#include "pooling_tb.h"
-#include "fully_connected_tb.h"
+#include <fstream>
+#include <stdexcept>
+
+#include "../design/template/constants.h"
+
+#define IMAGE_PATH "/home/kotfu/BSCThesis/data/t10k-images.idx3-ubyte"
+#define LABEL_PATH "/home/kotfu/BSCThesis/data/t10k-labels.idx1-ubyte"
+#define WEIGHT_PATH "/home/kotfu/BSCThesis/Weights/test_weights.wgt"
+
+#define TEST_COUNT 10
+
+
+int reverseBits(int n) {
+//        n = (n >> 1) & 0x55555555 | (n << 1) & 0xaaaaaaaa;
+//        n = (n >> 2) & 0x33333333 | (n << 2) & 0xcccccccc;
+//        n = (n >> 4) & 0x0f0f0f0f | (n << 4) & 0xf0f0f0f0;
+        n = (n >> 8) & 0x00ff00ff | (n << 8) & 0xff00ff00;
+        n = (n >> 16) & 0x0000ffff | (n << 16) & 0xffff0000;
+        return n;
+}
+
+void read_image(hls::stream< DATA_TYPE[1] > &input, int number_of_images=TEST_COUNT){
+	printf("Reading image from %s\n", IMAGE_PATH);
+	std::ifstream in_image(IMAGE_PATH);
+	if(!in_image.is_open()) throw std::runtime_error("Image file failed to open");
+
+	int magic;
+	in_image.read((char*)&magic,sizeof(magic));
+	magic = reverseBits(magic);
+	if(magic != 2051) throw std::runtime_error("Invalid MNIST image file!");
+
+	int image_counter;
+	in_image.read((char*)&image_counter,sizeof(image_counter));
+	image_counter = reverseBits(image_counter);
+
+	int height;
+	in_image.read((char*)&height,sizeof(height));
+	height = reverseBits(height);
+
+	int width;
+	in_image.read((char*)&width,sizeof(width));
+	width = reverseBits(width);
+
+	unsigned char temp_value;
+	DATA_TYPE value[1];
+	for(int image_counter=0; image_counter<width;image_counter++){
+		for(int w=0; w<width;w++){
+			for(int h=0; h<height;h++){
+				in_image >> temp_value;
+				value[0]=temp_value;
+				input.write(value);
+			}
+		}
+	}
+}
+
+void read_weight(hls::stream<WEIGHT> &weight, int number_of_weights){
+////	for(std::string line; std::getline(in_weights, line);){
+//
+//		float temp_weight;
+//
+//		std::sscanf( line.c_str(), "%f", &temp_weight );
+//
+//		My_Temlpate_Struct load;
+//		load.weight = temp_weight*(1 << 15);
+//		load.last = 0;
+//
+////		printf("Weight: %f, struct_weight: %d\n",temp_weight,load.weight);
+//
+//		templ.write(load);
+//
+////		printf("type: %s, value: %f\n", typeid(weight).name(),weight);
+//
+//	}
+//
+//	My_Temlpate_Struct load;
+//	load.weight = 0;
+//	load.last = 1;
+//	templ.write(load);
+////	printf("%d Weight loaded\n", i);
+
+
+}
+
+void convolution_test(){
+	hls::stream< DATA_TYPE[1] > input("conv_test_input");
+	hls::stream< DATA_TYPE[32] > output("conv_test_output");
+	hls::stream<WEIGHT> &weight("conv_test_weight");
+	hls::stream<WEIGHT> &weight_forward("conv_test_weight_forward");
+
+	read_image(input,1);
+	read_weight(weight, 28*28*1*32);
+
+	convolution_template<28,28,1,3,32>(input,output,weight,weight_forward,true);
+
+	for(int c=0;c<28*28;c++){
+		output.read();
+	}
+	printf("Convolution test success.\n");
+}
+
 
 int main(){
-//	printf("%s test done.\n",convolution_test().c_str()); // nem jó még
+	convolution_test();
+
+
+
 	printf("%s test done.\n",pooling_test().c_str()); // Nem csinál semmit
 	printf("%s test done.\n",fully_connected_test().c_str()); // Nem csinál semmit
 	return 0;
